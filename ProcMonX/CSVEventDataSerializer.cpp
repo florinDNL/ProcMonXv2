6ebe2853037ba5eea59df5c7b5e561006f5f440a
florinDNL <dalianflorin@gmail.com>
2025-06-21 19:00:46 +0300

    Implement CSV serialization

--- ProcMonX/CSVEventDataSerializer.cpp
+++ ProcMonX/CSVEventDataSerializer.cpp
@@ -1,16 +1,63 @@
 #include "pch.h"
 #include "CSVEventDataSerializer.h"
 #include <fstream>
+#include <format>
+#include "FormatHelper.h"
+
+
+//  Needed for processes like msedge which contain commans in their command line, messing up the csv formatting
+std::wstring CSVEventDataSerializer::EscapeCommandLine(std::wstring cmd)
+{
+    std::wstring escapedCmd = L"";
+    for (wchar_t c : cmd)
+    {
+        if (c == '\"')
+        {
+            escapedCmd += c;
+        }
+        escapedCmd += c;
+    }
+
+    return std::format(L"\"{}\"", escapedCmd);
+}
+
+std::wstring CSVEventDataSerializer::SerializeEvent(std::shared_ptr<EventData>& event)
+{
+    std::wstring ts = (std::wstring)FormatHelper::FormatTime(event->GetTimeStamp());
+    std::wstring name = event->GetEventName();
+    std::wstring cmd, status, parentId = L"";
+    CString img = L"";
+
+    if (name == L"Process/Start" || name == L"Process/End")
+    {
+        cmd = (std::wstring)(event->GetProperty(L"CommandLine")->GetUnicodeString());
+        img = event->GetProperty(L"ImageFileName")->GetAnsiString();
+        status = std::to_wstring(event->GetProperty(L"ExitStatus")->GetValue<DWORD>());
+        parentId = std::to_wstring(event->GetProperty(L"ParentId")->GetValue<DWORD>());
+    }
+
+    std::wstring serial = std::format(L"{},{},{},{},{},{},{},{},{},{}", event->GetIndex(), ts, name, 
+                                      event->GetProcessName(), event->GetProcessId(), event->GetThreadId(),
+                                      EscapeCommandLine(cmd), (std::wstring)img, status, parentId);
+    
+    return serial;
+}
 
 bool CSVEventDataSerializer::Save(const std::vector<std::shared_ptr<EventData>>& events, const EventDataSerializerOptions& options, PCWSTR path) {
-    std::ofstream out;
+    std::wofstream out;
     out.open(path);
     if(out.fail())
         return false;
 
-    for (auto& evt : events) {
+    out << L"Id,Time Stamp,Event Name,Process Name,PID,TID,Command Line,Image Name,Exit Status,Parent ID" << std::endl;
+
+    for (std::shared_ptr<EventData> evt : events) {
+        out << SerializeEvent(evt) << std::endl;
     }
-    return false;
+
+    out.close();
+
+    return true;
 }
 
 std::vector<std::shared_ptr<EventData>> CSVEventDataSerializer::Load(PCWSTR path) {
--- ProcMonX/CSVEventDataSerializer.h
+++ ProcMonX/CSVEventDataSerializer.h
@@ -2,8 +2,11 @@
 #include "IEventDataSerializer.h"
 
 class CSVEventDataSerializer : public IEventDataSerializer {
+private:
+	std::wstring SerializeEvent(std::shared_ptr<EventData>& event);
+	std::wstring EscapeCommandLine(std::wstring cmd);
 public:
 	virtual bool Save(const std::vector<std::shared_ptr<EventData>>& events, const EventDataSerializerOptions& options, PCWSTR path) override;
-	virtual std::vector<std::shared_ptr<EventData>> Load(PCWSTR path) override;
+	virtual std::vector<std::shared_ptr<EventData>> Load(PCWSTR path) override;	
 };
 
